4463a446ced02ce8be793d9dc67402bbbbcf389e
hawks <h.kareemy@gmail.com>
2022-02-07 21:41:46 -0800

    another try to commmit the project

---
+++ Java Basics/src/first_code/ExampleOfFirstCode.java
@@ -0,0 +1,17 @@
+package first_code;
+
+public class ExampleOfFirstCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10;
+		int b = 7;
+		b++; 
+		
+		
+		System.out.println(a+b);
+
+	}
+
+}
